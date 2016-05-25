cb978f59b56355a1890c57c1bafd42abdab34c9d
mraad@esri.com <mraad@esri.com>
2016-05-24 21:05:59 -0700

    Fixed README typoes

--- curl-put.sh
+++ curl-put.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 pushd /tmp
 curl -O http://ftp.dot.state.tx.us/pub/txdot-info/tpp/roadway-inventory/2014.zip
-unzip 2014.zip
+unzip 2014.zip TXDOT_Roadway_Inventory.gdb/*
 hadoop fs -put TXDOT_Roadway_Inventory.gdb
 popd
 awk -f points.awk | hadoop fs -put - points.csv
